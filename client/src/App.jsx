fdb1993b4ff001bc75d93e0d98a98bafdb76c382
Gayathri Gamage <99193625+gayathrip2000@users.noreply.github.com>
2023-11-22 19:14:40 +0530

    Second Commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Home from './pages/Home'
+import Sigin from "./pages/Sigin"
+import SignUp from './pages/SignUp'
+import About from "./pages/About"
+import Profile from "./pages/Profile"
+import Header from "./components/Header"
+
 
 export default function App() {
   return (
-    <div>App</div>
+
+    <BrowserRouter>
+
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-in" element={<Sigin />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile/>} />
+    </Routes>
+    </BrowserRouter>
   )
 }
