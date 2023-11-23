e8247162c6eaf1221d173d5af1463efb15080fe3
Gayathri Gamage <99193625+gayathrip2000@users.noreply.github.com>
2023-11-24 00:48:21 +0530

    Fourth Commit

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,14 +1,35 @@
-import User from '../models/user.model.js';
-import bcryptjs from 'bcryptjs';
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import Jwt from "jsonwebtoken";
 
-export const signup = async (req, res) => {
-    try {
-        const { username, email, password } = req.body;
-        const hashedPassword = await bcryptjs.hash(password, 10); // Await the hash result
-        const newUser = new User({ username, email, password: hashedPassword });
-        await newUser.save();
-        res.status(201).json('User Created');
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+export const signup = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+    const hashedPassword = await bcryptjs.hash(password, 10); // Await the hash result
+    const newUser = new User({ username, email, password: hashedPassword });
+    await newUser.save();
+    res.status(201).json("User Created");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Invalid password"));
+    const token = Jwt.sign({ _id: validUser._id }, process.env.JWT_SECRET);
+    const{password :pass, ...rest} = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
