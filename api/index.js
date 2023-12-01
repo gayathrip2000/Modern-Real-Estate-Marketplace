540ad0174178565627f0f0dc428be3bdb87631e1
Gayathri Gamage <99193625+gayathrip2000@users.noreply.github.com>
2023-12-01 12:16:09 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
 
@@ -17,7 +18,8 @@ mongoose
     console.log(err);
   });
 
- 
+
+  const __dirname = path.resolve();
 
 const app = express();
 
@@ -31,6 +33,11 @@ app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 
 app.use((err, req, res, next) => {
